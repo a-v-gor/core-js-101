ab7d06c9fc5de63b5b8bd3b0783537be81f47fa5
Alexey Gorbenko <a.v.gor@mail.ru>
2023-05-30 18:01:47 +0300

    feat: add ddecisions for date-tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,12 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  if (!(year % 100)) {
+    return !(year % 400);
+  }
+  return !(year % 4);
 }
 
 
@@ -73,8 +77,30 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let diff = endDate - startDate;
+  let hours = 0;
+  let mins = 0;
+  let secs = 0;
+  function returnTwoDigits(num) {
+    return `0${num}`.slice(-2);
+  }
+  function returnThreeDigits(num) {
+    return `00${num}`.slice(-3);
+  }
+  if (diff >= 3600000) {
+    hours = Math.floor(diff / 3600000);
+    diff -= hours * 3600000;
+  }
+  if (diff >= 60000) {
+    mins = Math.floor(diff / 60000);
+    diff -= mins * 60000;
+  }
+  if (diff >= 1000) {
+    secs = Math.floor(diff / 1000);
+    diff -= secs * 1000;
+  }
+  return `${returnTwoDigits(hours)}:${returnTwoDigits(mins)}:${returnTwoDigits(secs)}.${returnThreeDigits(diff)}`;
 }
 
 
@@ -94,8 +120,16 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hour = date.getUTCHours() % 12;
+  const minute = date.getMinutes();
+  const hourAngle = hour * 30 + minute * 0.5;
+  const minuteAngle = minute * 6;
+  let diff = Math.abs(minuteAngle - hourAngle);
+  if (diff > 180) {
+    diff = 360 - diff;
+  }
+  return Math.PI * (diff / 180);
 }
 
 
